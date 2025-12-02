6c6996de812b6642d524cc868f3fab15927eb251
g-99215544-beep <g-99215544@moe-dl.edu.my>
2025-12-02 15:46:16 +0800

    Add Firebase configuration and record management functions

---
+++ database.js
@@ -0,0 +1,50 @@
+// database.js
+// Konfigurasi Firebase yang tuan beri
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbCgDz2vK2BZUpwM3iDWJcPQSptVcNkv4",
+  authDomain: "kehadiran-murid-6ece0.firebaseapp.com",
+  databaseURL: "https://kehadiran-murid-6ece0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kehadiran-murid-6ece0",
+  storageBucket: "kehadiran-murid-6ece0.firebasestorage.app",
+  messagingSenderId: "223849234784",
+  appId: "1:223849234784:web:e1471ded7ea17ba60bde05",
+  measurementId: "G-4DY138HKTW"
+};
+
+// Inisialisasi Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.database();
+
+// Simpan satu rekod
+function saveRecordToFirebase(record) {
+  const ref = db.ref("rekod_pemantauan");
+  return ref.push(record);
+}
+
+// Dengar semua rekod untuk tarikh tertentu (contoh: hari ini)
+function subscribeToTodayRecords(tarikh, callback) {
+  const query = db
+    .ref("rekod_pemantauan")
+    .orderByChild("tarikh")
+    .equalTo(tarikh);
+
+  query.on("value", (snapshot) => {
+    const results = [];
+    snapshot.forEach((child) => {
+      results.push({
+        id: child.key,
+        ...child.val()
+      });
+    });
+    callback(results);
+  });
+}
+
+// Optional: kalau perlu hentikan listener kemudian
+function unsubscribeTodayRecords() {
+  db.ref("rekod_pemantauan").off();
+}
