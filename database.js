bb22b34aef60bb3a8b859d44361f0267d087767b
g-99215544-beep <g-99215544@moe-dl.edu.my>
2025-12-03 07:59:08 +0800

    Add documentation for Firebase record functions
    Added documentation comments for functions related to saving and retrieving records in Firebase, including structure and parameters.

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 // database.js
+// Firebase config + fungsi rekod pemantauan + bacaan kehadiran harian
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDbCgDz2vK2BZUpwM3iDWJcPQSptVcNkv4",
@@ -18,13 +20,27 @@ if (!firebase.apps.length) {
 
 const db = firebase.database();
 
-// Simpan satu rekod
+/**
+ * Simpan satu rekod pemantauan
+ * Struktur:
+ *  rekod_pemantauan/{autoId} : {
+ *    tarikh: "dd/mm/yyyy",
+ *    waktu: "W1" ... "W12" / "Luar Waktu PdP",
+ *    masaRekod: "HH:MM:SS",
+ *    namaGuru, kelas, tempat, jumlahMurid,
+ *    namaMurid, jenis ("baik"/"jahat"/null),
+ *    kategori, keterangan
+ *  }
+ */
 function saveRecordToFirebase(record) {
   const ref = db.ref("rekod_pemantauan");
   return ref.push(record);
 }
 
-// Dengar semua rekod untuk tarikh tertentu
+/**
+ * Dengar semua rekod untuk tarikh tertentu (dd/mm/yyyy)
+ * callback akan terima array rekod dengan id
+ */
 function subscribeToTodayRecords(tarikh, callback) {
   const query = db
     .ref("rekod_pemantauan")
@@ -43,7 +59,23 @@ function subscribeToTodayRecords(tarikh, callback) {
   });
 }
 
-// Hentikan semua listener (guna bila tukar tarikh)
+/**
+ * Hentikan semua listener (guna bila tukar tarikh semak rekod)
+ */
 function unsubscribeTodayRecords() {
   db.ref("rekod_pemantauan").off();
+}
+
+/**
+ * Ambil rekod kehadiran harian untuk satu kelas
+ * - dateKey: "YYYY-MM-DD" (sama seperti modul kehadiran murid)
+ * - kelas: contoh "5B"
+ * Path di Firebase: kehadiran/{dateKey}/{kelas}
+ */
+function getAttendanceForClass(dateKey, kelas) {
+  const path = "kehadiran/" + dateKey + "/" + kelas;
+  return db
+    .ref(path)
+    .once("value")
+    .then((snapshot) => snapshot.val());
 }
