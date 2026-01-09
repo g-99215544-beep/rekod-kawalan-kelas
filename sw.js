7cce4c41bde4c34974b499f17e80b234b9e6890f
g-99215544-beep <g-99215544@moe-dl.edu.my>
2026-01-09 14:37:25 +0800

    Refactor service worker caching strategy

--- sw.js
+++ sw.js
@@ -1,21 +1,58 @@
+const CACHE_NAME = 'sksa-pemantauan-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './app-icon-192.png',
+  './app-icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap'
+];
+
+// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('sksa-cache-v1').then(cache => {
-      return cache.addAll([
-        '/index_full.html',
-        '/database.js',
-        '/manifest.json',
-        '/app-icon-192.png',
-        '/app-icon-512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache dibuka');
+        return cache.addAll(urlsToCache);
+      })
   );
+  self.skipWaiting();
 });
 
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Padam cache lama:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - Network first, fallback to cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        // Clone response untuk cache
+        if (response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
